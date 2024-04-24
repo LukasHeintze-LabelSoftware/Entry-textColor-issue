59c8a5bc193847653a36614126bed86f828044d5
Lukas Heintze <1995-l@live.de>
2024-04-24 12:25:26 +0200

    initial commit

---
+++ MauiAppTest/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppTest;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        TestProp = "This is a Test";
+    }
+
+    public string TestProp { get; set; }
+}
